c6db37dedf7e964153afbb7c00dea35fd55865e9
dandritch <deivh@yahoo.fr>
2021-07-02 09:59:16 +0200

    add table view

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <FilesTable />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from "./components/HelloWorld.vue";
+import FilesTable from "./components/FilesTable.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    FilesTable,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/FilesTable.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="table">
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Size</th>
+        <th>Date</th>
+        <th>Actions</th>
+      </tr>
+      <tr v-for="file in dfiles" :key="file.name">
+        <td>{{ file.name }}</td>
+        <td>{{ file.size }}</td>
+        <td>{{ file.date }}</td>
+        <td>
+          <button>VIEW</button>
+          <button>RENAME</button>
+          <button>DELETE</button>
+
+          <button>DOWNLOAD</button>
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FilesTable",
+  data() {
+    return {
+      dfiles: [
+        {
+          path: "photos/summer/june/windsurf.jpg",
+          name: "windsurf.jpg",
+          size: 400,
+          type: "file",
+          extension: ".jpg",
+        },
+        {
+          path: "photos/winter",
+          name: "winter",
+          size: 200,
+          type: "directory",
+        },
+        {
+          path: "photos/winter/january",
+          name: "january",
+          size: 200,
+          type: "directory",
+        },
+        {
+          path: "photos/winter/january/ski.png",
+          name: "ski.png",
+          size: 100,
+          type: "file",
+          extension: ".png",
+        },
+        {
+          path: "photos/winter/january/snowboard.jpg",
+          name: "snowboard.jpg",
+          size: 100,
+          type: "file",
+          extension: ".jpg",
+        },
+      ],
+    };
+  },
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
